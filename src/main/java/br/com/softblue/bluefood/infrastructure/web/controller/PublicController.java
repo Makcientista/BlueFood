71e147dac8cec9068b45516d8655cbf258681c96
makiesse <ddociel@hotmail.com>
2021-09-22 02:44:12 -0300

    Persisting the customer in the database

--- src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
@@ -1,10 +1,14 @@
 package br.com.softblue.bluefood.infrastructure.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.softblue.bluefood.application.ClienteService;
 import br.com.softblue.bluefood.domain.cliente.Cliente;
 
 
@@ -12,12 +16,19 @@ import br.com.softblue.bluefood.domain.cliente.Cliente;
 @RequestMapping(path = "/public") // (path = "/public") = calls classe
 public class PublicController {
     
+	@Autowired
+	private ClienteService clienteService;
+	
 	@GetMapping("/cliente/new")//respond get oprations in this url ("/cliente/new") = call method newCliente
 	public String newCliente(Model model ) {		
-		Cliente eu = new Cliente();
-		eu.setNome("Makiesse");
+		model.addAttribute("cliente", new  Cliente()); //Cliente cliente = new  Cliente(); //model.addAttribute("cleinte", cliente);
+		return "cliente-cadastro";
 		
-		model.addAttribute("cliente", eu); //Cliente cliente = new  Cliente(); //model.addAttribute("cleinte", cliente);
+	}
+	
+	@PostMapping(path = "/cliente/save")
+	public String saveCliente(@ModelAttribute("cliente") Cliente cliente){
+		clienteService.saveCliente(cliente);
 		return "cliente-cadastro";
 		
 	}
