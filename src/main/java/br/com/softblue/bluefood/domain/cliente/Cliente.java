1db99e6a322ea02dd30f4ab06b0f34ef7612b94b
makiesse <marinafilomena-17@hotmail.com>
2021-11-11 13:23:21 -0300

    Encrypting the customer password

--- src/main/java/br/com/softblue/bluefood/domain/cliente/Cliente.java
+++ src/main/java/br/com/softblue/bluefood/domain/cliente/Cliente.java
@@ -1,12 +1,14 @@
 package br.com.softblue.bluefood.domain.cliente;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 import br.com.softblue.bluefood.domain.usuario.Usuario;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
-
 
 @Getter
 @Setter
@@ -15,7 +17,15 @@ import lombok.Setter;
 @SuppressWarnings("serial")
 public class Cliente extends Usuario {
 	
+
+	@NotBlank(message = "O CPF não pode ser vazio")
+	@Pattern(regexp = "[0-9]{10,11}", message = "O CPF possui formato inválido")
+	@Column(length = 11, nullable = false)
 	private String cpf;
 	
+	@NotBlank(message = "O CEP não pode ser vazio")
+	@Pattern(regexp = "[0-9]{8}", message = "O CEP possui formato inválido")
+	@Column(length = 8)
 	private String cep;
+	
 }
