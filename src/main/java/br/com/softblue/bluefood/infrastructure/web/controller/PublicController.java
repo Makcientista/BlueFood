04c99744666f8cceeca6f88a6f56941d0da6b387
makiesse <marinafilomena-17@hotmail.com>
2021-11-11 14:05:52 -0300

    Revert "Revert "Encrypting the customer password""
    This reverts commit 3f745523e67c1f721f24aa6dacfca82cb78d6044.

--- src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
+++ src/main/java/br/com/softblue/bluefood/infrastructure/web/controller/PublicController.java
@@ -1,14 +1,18 @@
 package br.com.softblue.bluefood.infrastructure.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.softblue.bluefood.application.ClienteService;
+import br.com.softblue.bluefood.application.ValidationException;
 import br.com.softblue.bluefood.domain.cliente.Cliente;
 
 
@@ -19,17 +23,31 @@ public class PublicController {
 	@Autowired
 	private ClienteService clienteService;
 	
-	@GetMapping("/cliente/new")//respond get oprations in this url ("/cliente/new") = call method newCliente
+	@GetMapping("/cliente/new")//respond get operations in this url ("/cliente/new") = call method newCliente
 	public String newCliente(Model model ) {		
 		model.addAttribute("cliente", new  Cliente()); //Cliente cliente = new  Cliente(); //model.addAttribute("cleinte", cliente);
+		ControllerHelper.setEditMode(model, false);
 		return "cliente-cadastro";
 		
 	}
 	
 	@PostMapping(path = "/cliente/save")
-	public String saveCliente(@ModelAttribute("cliente") Cliente cliente){
-		clienteService.saveCliente(cliente);
-		return "cliente-cadastro";
+	public String saveCliente(@ModelAttribute("cliente") @Valid Cliente cliente, 
+			Errors errors, 
+			Model model){
 		
+		if (!errors.hasErrors()) {
+			try {
+			clienteService.saveCliente(cliente);
+			model.addAttribute("msg", "Cliente gravado com sucesso!");
+			
+		} catch (ValidationException e) {
+			errors.rejectValue("email", null, e.getMessage());
+		}
+		}
+		ControllerHelper.setEditMode(model, false);
+		return "cliente-cadastro";
+			}
 	}
-}
+	
+
