3f745523e67c1f721f24aa6dacfca82cb78d6044
makiesse <marinafilomena-17@hotmail.com>
2021-11-11 13:55:16 -0300

    Revert "Encrypting the customer password"
    This reverts commit 1db99e6a322ea02dd30f4ab06b0f34ef7612b94b.

--- src/main/java/br/com/softblue/bluefood/domain/usuario/Usuario.java
+++ src/main/java/br/com/softblue/bluefood/domain/usuario/Usuario.java
@@ -2,22 +2,11 @@ package br.com.softblue.bluefood.domain.usuario;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
-import org.hibernate.annotations.BatchSize;
-import org.hibernate.annotations.NotFound;
-
-import com.sun.el.parser.AstFalse;
-
-import br.com.softblue.bluefood.util.StringUtils;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -34,28 +23,13 @@ public class Usuario implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	
-	@NotBlank(message="O nome não pode ser vazio")
-	@Size(max=80, message = "o nome é muito grande")
 	private String nome;
 	
-	@NotBlank(message = "O e-mail não pode ser vazio")
-	@Size(max = 60, message = "o e-mail é muito grande")
-	@Email(message="O e-mail é invalido")
 	private String email;
 	
-	@NotBlank(message = "A senha não pode ser vazio")
-	@Size(max = 80, message = "a senha é muito grande")
 	private String senha;
 	
-	@NotBlank(message = "O telefone não pode ser vazio")
-	@Pattern(regexp = "[0-9]{10,11}", message = "O telefone possui formato invalido")
-	@Column(length = 11, nullable = false )
 	private String telefone;
-	
-	public void encryptPassword() {
-		this.senha = StringUtils.encrypt(this.senha);
-		
-	}
 	
 	
 }
